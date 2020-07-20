7b380a202ab807e9cab19144813f5cba5cec7592
puneet <puneet15159@iiitd.ac.in>
2020-07-20 19:26:55 +0530

    [puneet] Added edit details

--- AddressBook/Person.cs
+++ AddressBook/Person.cs
@@ -14,5 +14,85 @@ namespace AddressBook
         private string zip;
         private string phoneNumber;
 
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+
+        public string GetLastNamee()
+        {
+            return lastName;
+        }
+
+        public void SetLastName(string _lastName)
+        {
+            lastName = _lastName;
+        }
+
+        public string Getaddress()
+        {
+            return address;
+        }
+
+        public void Setaddress(string _address)
+        {
+            address = _address;
+        }
+
+        public string GetCity()
+        {
+            return city;
+        }
+
+        public void SetCity(string _city)
+        {
+            city = _city;
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+
+        public void SetState(string _state)
+        {
+            state = _state;
+        }
+
+        public string GetZip()
+        {
+            return zip;
+        }
+
+        public void SetZip(string _zip)
+        {
+            zip = _zip;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public void SetPhoneNumber(string _phoneNumber)
+        {
+            phoneNumber = _phoneNumber;
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
@@ -7,6 +8,39 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            Person person = new Person("puneet", "M", "delhi", "delhi", "delhi", "110090", "123123123");
+
+            IDictionary<string, Person> addressBook = new Dictionary<string, Person>();
+
+            addToAddressBook(person, addressBook);
+
+            string name = "puneet";
+            string newCity = "goa";
+            string newZip = "110007";
+
+            editAddressBook(name,newCity,newZip,addressBook);
+
+
+
         }
+
+        private static void editAddressBook(string name, string newCity, string newZip, IDictionary<string, Person> addressBook)
+        {
+            Person person = new Person();
+            person = addressBook[name];
+            person.SetCity(newCity);
+            person.SetZip(newZip);
+            addressBook.Remove(name);
+            addressBook.Add(name, person);
+
+        }
+
+        private static void addToAddressBook(Person person, IDictionary<string, Person> addressBook)
+        {
+            addressBook.Add(person.GetFirstName(), person);
+        }
+
+
     }
 }
