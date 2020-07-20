d52ca075ef13e2d067aa96563730bf238142c632
puneet <puneet15159@iiitd.ac.in>
2020-07-20 18:39:31 +0530

    [puneet] Added person class

---
+++ AddressBook/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zip;
+        private string phoneNumber;
+
+    }
+}
