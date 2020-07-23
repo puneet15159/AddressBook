5239ad650d9a43ed64051f90de668de3c9867902
puneet <puneet15159@iiitd.ac.in>
2020-07-23 15:47:17 +0530

    [puneet] Modified refractored whole code add ed utility classes

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            int checkOut = 0;
+
+            IDictionary<string, Person> addressBook = new Dictionary<string, Person>();
+            AddressBookUtility addressBookUtility = new AddressBookUtility();
+            do
+            {
+                Console.WriteLine("choice 1: Add Person Details ");
+                Console.WriteLine("choice 2: Editing Person Details ");
+                Console.WriteLine("choice 3: Delete  Person Details ");
+                Console.WriteLine("choice 4: Add Multiple Person");
+                Console.WriteLine("choice 5: check for duplicate");
+                Console.WriteLine("choice 6: Short Persion By City");
+                Console.WriteLine("choice 7: Short Persion By Zip");
+                Console.WriteLine("choice 8: View person by city");
+                Console.WriteLine("choice 9: view Person by state");
+                Console.WriteLine("choice 10: Exit From here");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBook=addressBookUtility.addToAddressBook(addressBook);
+                        addressBookUtility.DisplayAddressBook(addressBook);
+                        break;
+                    case 2:
+                        addressBook=addressBookUtility.editAddressBook(addressBook);
+                        addressBookUtility.DisplayAddressBook(addressBook);
+                        break;
+                    case 3:
+                        addressBook=addressBookUtility.deleteFromAddressBook(addressBook);
+                        addressBookUtility.DisplayAddressBook(addressBook);
+                        break;
+                    case 4:
+                        addressBook=addressBookUtility.AddMultiple(addressBook);
+                        addressBookUtility.DisplayAddressBook(addressBook);
+                        break;
+                    case 5:
+                        bool checkDuplicate = addressBookUtility.checkDuplicate(addressBook);
+                        Console.WriteLine(checkDuplicate);
+                        break;
+                    case 6:
+                        addressBookUtility.sortByName(addressBook);
+                        break;
+                    case 7:
+                        addressBookUtility.SortByCity(addressBook);
+                        break;
+                    case 8:
+                        addressBookUtility.viewByCity(addressBook);
+                        break;
+                    case 9:
+                        addressBookUtility.searchInCity(addressBook);
+                        break;
+                    default:
+                        checkOut = 1;
+                        break;
+                }
+            } while (checkOut == 0);
+            addressBookUtility.DisplayAddressBook(addressBook);
+                
+        } 
+        
+    }
+}
---
+++ AddressBook/AddressBookUtility.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+
+namespace AddressBook
+{
+    internal class AddressBookUtility
+    {
+        internal IDictionary<string, Person> addToAddressBook(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the first Name of person:");
+            String fName = Console.ReadLine();
+            Console.WriteLine("Enter the last Name of person:");
+            String lName = Console.ReadLine();
+            Console.WriteLine("Enter the address");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter the city");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter the state");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter the zip");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter the phone number");
+            String phoneNumber = Console.ReadLine();
+            Person personNew = new Person(fName, lName, address, city, state, zip, phoneNumber);
+
+            addressBook.Add(personNew.GetFirstName(), personNew);
+
+            return addressBook;
+        }
+
+        internal IDictionary<string, Person> editAddressBook(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the phone number");
+            String phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the first name");
+            String fName = Console.ReadLine();
+
+            addressBook[fName].SetPhoneNumber(phoneNumber);
+
+            return addressBook;
+            
+
+        }
+
+        internal IDictionary<string, Person> deleteFromAddressBook(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the name to be deleted");
+            String name = Console.ReadLine();
+
+            addressBook.Remove(name);
+            return addressBook;
+        }
+
+        internal void DisplayAddressBook(IDictionary<string, Person> addressBook)
+        {
+
+            for (int i = 0; i < addressBook.Count; i++)
+            {
+                   Console.WriteLine("Key: {0}",addressBook.Keys.ElementAt(i));
+                   DisplayPerson(addressBook[addressBook.Keys.ElementAt(i)]);
+            }
+        }
+
+        internal IDictionary<string, Person> AddMultiple(IDictionary<string, Person> addressBook)
+        {
+            addressBook=addToAddressBook(addressBook);
+            addressBook=addToAddressBook(addressBook);
+            addressBook=addToAddressBook(addressBook);
+
+            return addressBook;
+
+            
+        }
+
+        internal bool checkDuplicate(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the name to be checked");
+            String name = Console.ReadLine();
+
+            return addressBook.ContainsKey(name);
+        }
+
+        internal void sortByName(IDictionary<string, Person> addressBook)
+        {
+            var list = addressBook.Keys.ToList();
+
+            foreach (var key in list)
+            {
+                Console.WriteLine(key, addressBook[key]);
+            }
+        }
+
+        internal void searchInCity(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the name");
+            String name = Console.ReadLine();
+            string city = addressBook[name].GetCity();
+            Console.WriteLine(city);
+        }
+
+        internal void viewByCity(IDictionary<string, Person> addressBook)
+        {
+            Console.WriteLine("Enter the city");
+            String city = Console.ReadLine();
+
+            for (int i = 0; i < addressBook.Count; i++)
+            {
+                if (addressBook[addressBook.Keys.ElementAt(i)].GetCity().Equals(city))
+                {
+                    Console.WriteLine("Key: {0}",
+                                                        addressBook.Keys.ElementAt(i));
+                    
+                    DisplayPerson(addressBook[addressBook.Keys.ElementAt(i)]);
+                }
+
+            }
+        }
+
+        private void DisplayPerson(Person person)
+        {
+            Console.WriteLine(person.GetFirstName());
+            Console.WriteLine(person.GetLastNamee());
+            Console.WriteLine(person.Getaddress());
+            Console.WriteLine(person.GetCity());
+            Console.WriteLine(person.GetZip());
+            Console.WriteLine(person.GetPhoneNumber());
+        }
+
+        internal void SortByCity(IDictionary<string, Person> addressBook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
